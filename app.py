bad52f62bc58c61383d9d894624418921470bd58
olela28 <130731715+olela28@users.noreply.github.com>
2024-09-07 07:18:43 +0000

    sqlite database

--- app.py
+++ app.py
@@ -6,6 +6,39 @@ from werkzeug.security import check_password_hash, generate_password_hash
 # Configure application
 app = Flask(__name__)
 
+# Custom filter
+app.jinja_env.filters["usd"] = usd
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"]= "filesystem"
+Session(app)
+
+# Configure cs50 library to use SQLite database
+db = SQL("sqlite:///finance.db")
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached!"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
 @app.route("/")
-def home():
-    return "Hello, Flask!"
+@login_required
+def index():
+    """Show portfolio of stocks"""
+
+    user_id = session["user_id"]
+
+    rows = db.execute("""
+                      SELECT cash, symbol,
+                      SUM(shares) AS total_shares
+                      FROM users
+                      LEFT JOIN transactions ON users.id = transactions.user_id
+                      WHERE users.id = ?
+                      GROUP BY symbol
+                      HAVING total_shares > 0 OR symbol is NULL""",
+                      user_id)
